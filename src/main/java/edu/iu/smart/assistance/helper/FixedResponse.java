1a368be84c69138b148110375df7b0b1fb0b3694
Sujeet Kumar <kumasuje@iu.edu>
2017-04-30 18:35:53 -0400

    Adding Gif resposne

--- src/main/java/edu/iu/smart/assistance/helper/FixedResponse.java
+++ src/main/java/edu/iu/smart/assistance/helper/FixedResponse.java
@@ -13,17 +13,21 @@ public class FixedResponse {
 
     public FixedResponse() {
 
-        fixedResponse.put("hey","/gif hey");
-        fixedResponse.put("hi","/gif hello");
-        fixedResponse.put("hello","/gif hi");
+        fixedResponse.put("hey","https://giphy.com/gifs/breakfast-egg-cook-3oGRFh9szq10x1HAys");
+        fixedResponse.put("hi","https://giphy.com/gifs/hello-hi-dzaUX7CAG0Ihi");
+        fixedResponse.put("hello","https://giphy.com/gifs/kZzY6eKKPdIjK");
 
-        fixedResponse.put("good night","/gif good night");
-        fixedResponse.put("goodnight","/gif good night");
-        fixedResponse.put("good day","/gif good day");
+        fixedResponse.put("good night","https://giphy.com/gifs/disney-disney-gif-pinocchio-jiminy-cricket-mguPrVJAnEHIY");
+        fixedResponse.put("goodnight","https://giphy.com/gifs/tvland-goodnight-4NEbxVNYIxraM");
+        fixedResponse.put("good day","https://giphy.com/gifs/cat-cute-haveagoodday-OojQx4M3zYUOk");
 
 
-        fixedResponse.put("bye","/gif bye");
-        fixedResponse.put("cya","/gif cya");
+        fixedResponse.put("bye","https://giphy.com/gifs/realitytvgifs-bye-married-to-medicine-dr-jackie-11krT7xA8uuLbG");
+        fixedResponse.put("cya","https://giphy.com/gifs/reactiongifs-rrLt0FcGrDeBq");
+        fixedResponse.put("whats up","https://giphy.com/gifs/robin-williams-wassup-eoVusT7Pi9ODe");
+        fixedResponse.put("sup","https://giphy.com/gifs/foxsports-nba-mood-jr-smith-TiytUXsnUDfsk");
+
+
     }
 
     Map<String,String> fixedResponse = new HashMap<String,String>();
